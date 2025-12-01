877fe2cc5c817c0b2e13d58b6ee67d21eb37f145
Huynh Van Luong <huynh.van.luong@apsensing.com>
2025-12-01 21:46:16 +0100

    upload modified files for lora

---
+++ scripts/finetuner.py
@@ -0,0 +1,69 @@
+from llava.train.train import train
+import argparse
+# from llava.model.builder import load_pretrained_model
+# from llava.mm_utils import get_model_name_from_path
+# from llava.eval.run_llava import eval_model
+
+# model_path = "liuhaotian/llava-v1.5-7b"
+
+# tokenizer, model, image_processor, context_len = load_pretrained_model(
+#     model_path=model_path,
+#     model_base=None,
+#     model_name=get_model_name_from_path(model_path)
+# )
+
+
+#!/bin/bash
+
+# IMPORTANT: this is the training script for the original LLaVA, NOT FOR LLaVA V1.5!
+
+# Uncomment and set the following variables correspondingly to run this script:
+
+################## VICUNA ##################
+PROMPT_VERSION = "v1"
+MODEL_VERSION = "vicuna-v1-3-7b"
+################## VICUNA ##################
+
+################## LLaMA-2 ##################
+# PROMPT_VERSION="llava_llama_2"
+# MODEL_VERSION="llama-2-7b-chat"
+################## LLaMA-2 ##################
+
+if __name__ == "__main__":
+    
+    # train(attn_implementation="flash_attention_2")
+    train()
+    
+# deepspeed llava/train/train_mem.py \
+#     --deepspeed ./scripts/zero2.json \
+#     --lora_enable True \
+#     --model_name_or_path ./checkpoints/$MODEL_VERSION \
+#     --version $PROMPT_VERSION \
+#     --data_path ./playground/data/llava_instruct_80k.json \
+#     --image_folder ./playground/data/coco/train2017 \
+#     --vision_tower openai/clip-vit-large-patch14 \
+#     --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
+#     --mm_vision_select_layer -2 \
+#     --mm_use_im_start_end False \
+#     --mm_use_im_patch_token False \
+#     --bf16 True \
+#     --output_dir ./checkpoints/llava-$MODEL_VERSION-finetune_lora \
+#     --num_train_epochs 1 \
+#     --per_device_train_batch_size 16 \
+#     --per_device_eval_batch_size 4 \
+#     --gradient_accumulation_steps 1 \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps 50000 \
+#     --save_total_limit 1 \
+#     --learning_rate 2e-5 \
+#     --weight_decay 0. \
+#     --warmup_ratio 0.03 \
+#     --lr_scheduler_type "cosine" \
+#     --logging_steps 1 \
+#     --tf32 False \
+#     --model_max_length 2048 \
+#     --gradient_checkpointing True \
+#     --lazy_preprocess True \
+#     --dataloader_num_workers 4 \
+#     --report_to wandb
